883b1d363e3882d1f8c6860ce6fccfd0895c28f7
xiaodianzheng <xdian.zheng@gmail.com>
2020-10-11 11:12:29 +0800

    complete question 1

--- AlgorithmAssignment3/A3Q1.py
+++ AlgorithmAssignment3/A3Q1.py
@@ -1,7 +1,33 @@
 import sys
 
+
 def LCMS(a, b):
-    return abs(len(a) - len(b)) + 2
+    """
+    The worst case time complexity is O(n^2) where n is min(len(A), len(B)) when A and B are identical.
+    """
+    common_sequence = []
+    for n in a:
+        if n in b:
+            common_sequence.append(int(n, 16))
+            b = b[b.index(n) + 1:]
+    n = len(common_sequence)
+    increase_indexes = [1] * n
+    decrease_indexes = [1] * n
+    for i in range(1, n):
+        for j in range(0, i):
+            if common_sequence[i] > common_sequence[j] \
+                    and increase_indexes[i] < increase_indexes[j] + 1:
+                increase_indexes[i] = increase_indexes[j] + 1
+    for i in range(n-2, -1, -1):
+        for j in range(i-1, n):
+            if common_sequence[i] > common_sequence[j] \
+                    and decrease_indexes[i] < decrease_indexes[j] + 1:
+                decrease_indexes[i] = decrease_indexes[j] + 1
+    res = 0
+    for i in range(1, n):
+        res = max(res, increase_indexes[i] + decrease_indexes[i] - 1)
+    return res
+
 
 num_pair = int(sys.stdin.readline())
 for _ in range(num_pair):
